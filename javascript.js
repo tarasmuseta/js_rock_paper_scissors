c087715af7a662da2b717e1f992bf36907bec5c3
Taras Museta <musetataras@gmail.com>
2022-06-17 21:52:13 +0200

    New index.html javascript.js

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const rock = 'Rock';
+
+const paper = 'Paper';
+
+const scissors = 'Scissors';
+
+const possibleResult = [
+    rock,
+    paper,
+    scissors
+];
+
+let opponentResult = possibleResult[Math.floor(Math.random()*possibleResult.length)];
+
