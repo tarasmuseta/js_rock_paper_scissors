b11eadaba4d8b2d71ec3c63a7889622180df6d99
Taras Museta <musetataras@gmail.com>
2022-06-18 11:38:48 +0200

    Cleaned formatting

--- javascript.js
+++ javascript.js
@@ -1,24 +1,12 @@
 function game () { 
 
     function capitalize (string) {
-
         let string1 = string.charAt(0).toUpperCase();
-
         let string2 = string.slice(1).toLowerCase();
-
-        userResult = string1 + string2;
-        
+        userResult = string1 + string2;    
     }
 
-    const possibleResult = [
- 
-        'Rock',
-
-        'Paper',
-
-        'Scissors'
-
-    ];
+    const possibleResult = ['Rock', 'Paper', 'Scissors'];
 
     let opponentResult = possibleResult[Math.floor(Math.random()*possibleResult.length)];
 
@@ -26,23 +14,27 @@ function game () {
 
     capitalize(userResult);
 
-    if (userResult !== ('Rock' || 'Paper' || 'Scissors')) {
-        
+    if (userResult !== ('Rock' || 'Paper' || 'Scissors')) { 
+
         alert('Try again! Please, only type words "Rock", "Paper" or "Scissors"!');
-
-        userResult = prompt("Make you choice!");
-        
+        userResult = prompt("Make you choice!");       
         capitalize(userResult);
-
+ 
     }
  
     alert(`Your opponent chose ${opponentResult}!`);
 
-    let Outcome1 = (userResult === 'Rock' && opponentResult === 'Scissors') || (userResult === 'Paper' && opponentResult === 'Rock') || (userResult === 'Scissors' && opponentResult === 'Paper');
+    let Outcome1 = (userResult === 'Rock' && opponentResult === 'Scissors') ||
+                    (userResult === 'Paper' && opponentResult === 'Rock') ||
+                    (userResult === 'Scissors' && opponentResult === 'Paper');
 
-    let Outcome2 = (userResult === 'Scissors' && opponentResult === 'Rock') || (userResult === 'Rock' && opponentResult === 'Paper') || (userResult === 'Paper' && opponentResult === 'Scissors');
+    let Outcome2 = (userResult === 'Scissors' && opponentResult === 'Rock') ||
+                    (userResult === 'Rock' && opponentResult === 'Paper') ||
+                    (userResult === 'Paper' && opponentResult === 'Scissors');
 
-    let Outcome3 = (userResult === 'Scissors' && opponentResult === 'Scissors') || (userResult === 'Rock' && opponentResult === 'Rock') || (userResult === 'Paper' && opponentResult === 'Paper');
+    let Outcome3 = (userResult === 'Scissors' && opponentResult === 'Scissors') ||
+                    (userResult === 'Rock' && opponentResult === 'Rock') ||
+                    (userResult === 'Paper' && opponentResult === 'Paper');
 
     if (Outcome1) {
 
