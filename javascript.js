c038f7999bfe2eaf25a2e56b06473c4f05ab0f3f
Taras Museta <musetataras@gmail.com>
2022-06-18 10:54:33 +0200

    Completed the algo

--- javascript.js
+++ javascript.js
@@ -1,42 +1,70 @@
-function capitalize (string) {
-    let string1 = string.charAt(0).toUpperCase();
-    let string2 = string.slice(1).toLowerCase();
-    alert(`You chose ${(string1 + string2)}`);
+function game () { 
 
-}
+    function capitalize (string) {
 
-const rock = 'Rock';
+        let string1 = string.charAt(0).toUpperCase();
 
-const paper = 'Paper';
+        let string2 = string.slice(1).toLowerCase();
 
-const scissors = 'Scissors';
+        userResult = string1 + string2;
+        
+    }
 
-rock < paper;
+    const possibleResult = [
+ 
+        'Rock',
 
-paper < scissors;
+        'Paper',
 
-scissors < rock;
+        'Scissors'
 
-const possibleResult = [
-    rock,
-    paper,
-    scissors
-];
+    ];
 
-let opponentResult = possibleResult[Math.floor(Math.random()*possibleResult.length)];
+    let opponentResult = possibleResult[Math.floor(Math.random()*possibleResult.length)];
 
-let userResult = prompt("Let's play Rock, Paper, Scissors! Make you choice!");
-
-if (userResult === rock | paper | scissors) {
+    let userResult = prompt("Let's play Rock, Paper, Scissors! Make you choice!");
 
     capitalize(userResult);
 
-} else {
+    if (userResult !== ('Rock' || 'Paper' || 'Scissors')) {
+        
+        alert('Try again! Please, only type words "Rock", "Paper" or "Scissors"!');
 
-    alert('Try again! Please only type words "Rock", "Paper" or "Scissors"!');
+        userResult = prompt("Make you choice!");
+        
+        capitalize(userResult);
 
-    userResult = prompt("Make you choice!");
+    }
+ 
+    alert(`Your opponent chose ${opponentResult}!`);
+
+    let Outcome1 = (userResult === 'Rock' && opponentResult === 'Scissors') || (userResult === 'Paper' && opponentResult === 'Rock') || (userResult === 'Scissors' && opponentResult === 'Paper');
+
+    let Outcome2 = (userResult === 'Scissors' && opponentResult === 'Rock') || (userResult === 'Rock' && opponentResult === 'Paper') || (userResult === 'Paper' && opponentResult === 'Scissors');
+
+    let Outcome3 = (userResult === 'Scissors' && opponentResult === 'Scissors') || (userResult === 'Rock' && opponentResult === 'Rock') || (userResult === 'Paper' && opponentResult === 'Paper');
+
+    if (Outcome1) {
+
+        alert(`You won! ${userResult} beats ${opponentResult}`);
+        console.log(`You won! ${userResult} beats ${opponentResult}`);
+
+    } else if (Outcome2) {
+
+        alert(`You lost! ${opponentResult} beats ${userResult}`);
+        console.log(`You lost! ${opponentResult} beats ${userResult}`);
+
+    } else if (Outcome3){
+
+        alert("It's a tie!");
+        console.log("It's a tie!");
+
+    }
 
 }
 
-alert(`Your opponent chose ${opponentResult}!`);
+for (let i = 0; i < 5; i++) {
+    
+    game();
+
+}
